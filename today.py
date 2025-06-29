cb3b1c7562cce894efebad65ed464dd20ff190e2
zhangjingdian <zjd@zhangjingdiandeMac-mini.local>
2025-06-30 00:55:01 +0800

    wow

--- today.py
+++ today.py
@@ -272,45 +272,41 @@ def cache_builder(edges, comment_size, force_cache, loc_add=0, loc_del=0):
     Checks each repository in edges to see if it has been updated since the last time it was cached
     If it has, run recursive_loc on that repository to update the LOC count
     """
-    
-    # 确保 cache 目录存在，以防在 Actions 环境中不存在
-    os.makedirs('cache', exist_ok=True)
-    
     cached = True # Assume all repositories are cached
     filename = 'cache/'+hashlib.sha256(USER_NAME.encode('utf-8')).hexdigest()+'.txt' # Create a unique filename for each user
     try:
         with open(filename, 'r') as f:
-            lines = f.readlines()
+            data = f.readlines()
     except FileNotFoundError: # If the cache file doesn't exist, create it
-        lines = []
+        data = []
         if comment_size > 0:
-            for _ in range(comment_size): lines.append('This line is a comment block. Write whatever you want here.\n')
+            for _ in range(comment_size): data.append('This line is a comment block. Write whatever you want here.\n')
         with open(filename, 'w') as f:
-            f.writelines(lines)
+            f.writelines(data)
 
-    if len(lines)-comment_size != len(edges) or force_cache: # If the number of repos has changed, or force_cache is True
+    if len(data)-comment_size != len(edges) or force_cache: # If the number of repos has changed, or force_cache is True
         cached = False
         flush_cache(edges, filename, comment_size)
         with open(filename, 'r') as f:
-            lines = f.readlines()
+            data = f.readlines()
 
-    cache_comment = lines[:comment_size] # save the comment block
-    lines = lines[comment_size:] # remove those lines
+    cache_comment = data[:comment_size] # save the comment block
+    data = data[comment_size:] # remove those lines
     for index in range(len(edges)):
-        repo_hash, commit_count, *__ = lines[index].split()
+        repo_hash, commit_count, *__ = data[index].split()
         if repo_hash == hashlib.sha256(edges[index]['node']['nameWithOwner'].encode('utf-8')).hexdigest():
             try:
                 if int(commit_count) != edges[index]['node']['defaultBranchRef']['target']['history']['totalCount']:
                     # if commit count has changed, update loc for that repo
                     owner, repo_name = edges[index]['node']['nameWithOwner'].split('/')
-                    loc = recursive_loc(owner, repo_name, lines, cache_comment)
-                    lines[index] = repo_hash + ' ' + str(edges[index]['node']['defaultBranchRef']['target']['history']['totalCount']) + ' ' + str(loc[2]) + ' ' + str(loc[0]) + ' ' + str(loc[1]) + '\n'
+                    loc = recursive_loc(owner, repo_name, data, cache_comment)
+                    data[index] = repo_hash + ' ' + str(edges[index]['node']['defaultBranchRef']['target']['history']['totalCount']) + ' ' + str(loc[2]) + ' ' + str(loc[0]) + ' ' + str(loc[1]) + '\n'
             except TypeError: # If the repo is empty
-                lines[index] = repo_hash + ' 0 0 0 0\n'
+                data[index] = repo_hash + ' 0 0 0 0\n'
     with open(filename, 'w') as f:
         f.writelines(cache_comment)
-        f.writelines(lines)
-    for line in lines:
+        f.writelines(data)
+    for line in data:
         loc = line.split()
         loc_add += int(loc[3])
         loc_del += int(loc[4])
